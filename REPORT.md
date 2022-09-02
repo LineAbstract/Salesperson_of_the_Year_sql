f8793fca66033dcdcc1d3bb4b1147b5d4db4f666
Remo Lanhaus <remo.lanhaus@gmail.com>
2022-09-02 12:38:14 -0700

    completed set query questions, completed REPORT.md with analysis and answering of questions prompted

--- REPORT.md
+++ REPORT.md
@@ -9,22 +9,48 @@ Your tone should be professional but informal: imagine that you are responding t
 ---
 
 1. Who was the top salesperson for the year?
-
+        - Our top salesperson of the year is Dirk Stringer. 
+        - He had 52 transactions and a total sales amount of $61,022,268.45.
 
 2. How much better did the top salesperson do than the individual who came in second for the year?
-
+        - Our top salesperson being Dirk Stringer with $61,022,268.45 in sales.
+        - Our second best salesperson Michel DeFrance with $36,413,772.33 in sales. 
+        - This is a difference of $24,608,496.12.
+        - Dirk Stringer sales volume is 1.6758 times or 167.58% more than Michel DeFrance. 
 
 3. Was there any relationship between the average sale amount for the top salespersons and the total sales?
-
+        - It does appear that a higher Average Sales Amount leads to a higher Total Sales Amount, but there are exceptions. 
+        - In the top 3 spots for Total Sales Amount, we see a direct correlation to the top 3 spots of highest Average Sales Amounts.
+        - The middle group also has a fairly strong apparent correlation, but there are outliers either with higher Total Sales and lower Average Sale Amount or the inverse, lower Total Sales and higher Average Sale Amount. 
+        - Most drastic outlier appears to be: 
+            - Meander Smith who has a low Total Sales Amount (bottom third), yet a high Average Sales Amount (top third)
 
 4. How do you think the top selling salesperson gained so many sales? What was their strategy?
-
-- Did they sell to many customers, or a dedicated few?
-- Did they tend to sell a few big ticket (high priced) items, or did they focus on a high volume of lower priced items?
-
- 
+        - Dirk sold to a dedicated few, for a total of 3 customers. CustomerID's: 9000, 9941, 18723.
+        - Dirk also sold a dedicated few items, for a total of 3 products. ProductID's: 90, 276, 297.
+                - Problem: Of the products Dirk sold, we have no record of Price (ProductID: 90) for the orders made to CustomerID 9000. 
+        - Of the remaining two products (ProductID: 276, 297), it's clear that they are of high value, $3,399.99 and $2443.35 respectively.
+        - It appears that Dirks strategy was to sell many high value products to just a few high value customers to get his numbers high.   
 
 5. What was the top selling product for the year? What was the lowest selling? (For both dollar amount sold and quantity sold).
-
+        - Top selling product by Quantity: 
+                - "Touring-3000 Blue, 54",
+                - 24175 sold,  
+                - ProductID: 463
+        - Top selling product by Dollar Amount: 
+                - "Mountain-100 Silver, 38", 
+                - $53,039,844 sold,
+                - ProductID: 276
+        - Lowest selling product by Quantity: 
+                - "Chain Stays",
+                - 169 sold, 
+                - ProductID: 13
+        - Lowest selling product by Dollar Amount (that we have data for): 
+                - "Adjustable Race"
+                - $849.60 sold,
+                - ProductID: 1
 
 6. What problems did you find in the data? (For example, where there any products without a price?)
+        - Multiple items without a price associated, which may have an effect on lowest seling product by dollar amount. 
+            - ProductIDs: 9, 26, 86, 90, 153
+        - Of the products Dirk sold, we have no record of Price (ProductID: 90) for the orders made to CustomerID 9000, which may increase his total sales number further. 
--- queries.sql
+++ queries.sql
@@ -1,35 +1,116 @@
 -- Test query & commit
-SELECT * FROM employees;
+-- SELECT * FROM employees;
+-- SELECT * FROM products;
+-- SELECT * FROM sales;
 
 -- Problem 1 & 2
 -- 1. Using JOINs in a single query, combine data from all three tables (employees, products, sales) to view all sales with complete employee and product information in one table.
+-- SELECT * FROM employees 
+-- JOIN sales ON employees.EmployeeID = sales.SalesPersonID
+-- JOIN products ON sales.ProductID = products.ProductID;
 
 -- 2a. Create a View for the query you made in Problem 1 named "all_sales"
 -- NOTE: You'll want to remove any duplicate columns to clean up your view!
-
+-- CREATE VIEW all_sales AS
+-- SELECT e.EmployeeID, e.FirstName, e.MiddleInitial, e.LastName, s.SalesID, s.CustomerID, p.ProductID, s.Quantity, p.Name AS ProductName, p.Price FROM employees e
+-- JOIN sales s ON e.EmployeeID = s.SalesPersonID
+-- JOIN products p ON s.ProductID = p.ProductID;
 
 -- 2b. Test your View by selecting all rows and columns from the View
+-- SELECT * FROM all_sales;
 
 -- Problem 3
 -- Find the average sale amount for each sales person
+-- SELECT 
+-- 		DISTINCT EmployeeID, FirstName, LastName,
+-- 		CONCAT('$ ', ROUND(SUM(Price * Quantity), 2)) AS TotalSalesAmount, 
+-- 		COUNT(EmployeeID) AS NumberofSales, 
+-- 		CONCAT('$ ',ROUND((SUM(Price * Quantity)/COUNT(EmployeeID)), 2)) AS AverageSaleAmount
+-- 	FROM all_sales
+-- GROUP BY EmployeeID
+-- ORDER BY EmployeeID ASC;
+
+-- SELECT 
+-- 		DISTINCT EmployeeID, FirstName, LastName,
+-- 		ROUND(SUM(Price * Quantity), 2) AS TotalSalesAmount, 
+-- 		COUNT(EmployeeID) AS NumberofSales, 
+-- 		ROUND((SUM(Price * Quantity)/COUNT(EmployeeID)), 2) AS AverageSaleAmount
+-- 	FROM all_sales
+-- GROUP BY EmployeeID
+-- ORDER BY EmployeeID ASC;
 
 -- Problem 4
 -- Find the top three sales persons by total sales
+-- SELECT 	EmployeeID, FirstName, LastName, 
+--         ROUND(SUM(Price * Quantity), 2) AS TotalSalesAmount
+-- 	FROM all_sales
+-- GROUP BY EmployeeID
+-- ORDER BY TotalSalesAmount DESC
+-- LIMIT 3;
 
 -- Problem 5
 -- Find the product that has the highest price
+-- SELECT ProductID, Name, Price FROM products
+-- ORDER BY PRICE DESC
+-- LIMIT 5;
 
 -- Problem 6
 -- Find the product that was sold the most times
+-- SELECT ProductID, Name, SUM(Quantity) AS NumberSold FROM all_sales
+-- GROUP BY ProductID
+-- ORDER BY NumberSold DESC
+-- LIMIT 1;
+
+-- Find the product that was sold the least times
+-- SELECT ProductID, Name, SUM(Quantity) AS NumberSold FROM all_sales
+-- GROUP BY ProductID
+-- ORDER BY NumberSold ASC
+-- LIMIT 1;
+
+-- Find the product with the highest dollar amount sold
+-- SELECT productID, Name, Price, Quantity, 
+-- 		ROUND(SUM(Price * Quantity), 2) AS TotalSalesAmount
+-- 	FROM all_sales
+-- GROUP BY productID
+-- ORDER BY TotalSalesAmount DESC;
+
+-- Find the product with the lowest dollar amount sold
+-- SELECT productID, Name, Price, Quantity, 
+-- 		ROUND(SUM(Price * Quantity), 2) AS TotalSalesAmount
+-- 	FROM all_sales
+-- GROUP BY productID
+-- ORDER BY TotalSalesAmount ASC;
 
 -- Problem 7
 -- Using a subquery, find all products that have a price higher than the average price for all products
+-- SELECT ProductID, Name, Price FROM products
+-- WHERE price > (
+-- 	SELECT AVG(Price) FROM products)
+-- ORDER BY Price ASC;
 
 -- Problem 8
 -- Find the customer who spent the most money in purchased products
+-- SELECT CustomerID, 
+-- 		ROUND(SUM(Quantity * Price), 2) AS CustomerSpend 
+-- 	FROM all_sales
+-- GROUP BY CustomerID
+-- ORDER BY CustomerSpend DESC;
 
 -- Problem 9
 -- Find the total number of sales for each sales person
+-- SELECT EmployeeID,
+-- 		COUNT(EmployeeID) AS NumberofSales
+-- 	FROM all_sales
+-- GROUP BY EmployeeID
+-- ORDER BY EmployeeID ASC;
 
 -- Problem 10
 -- Find the sales person who sold the most to the customer you found in Problem 8
+-- customer who spent the most money is CustomerID 18723, so I can simply select for that
+-- SELECT DISTINCT EmployeeID, FirstName, LastName FROM all_sales
+-- WHERE CustomerID = 18723
+
+-- Extra needed to answer REPORT question
+-- Which products, quantities of products and customers did the top salesperson sell? 
+-- SELECT FirstName, CustomerID, ProductID, Quantity, Price FROM all_sales
+-- WHERE FirstName = 'Dirk';
